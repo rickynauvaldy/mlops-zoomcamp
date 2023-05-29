52be74b564bbb56e4b92a5d7d9fe17868619c34a
Ricky Nauvaldy <ricky.nauvaldy@gmail.com>
2023-05-29 09:02:27 +0700

    [W2] Update train.py script to integrate mlflow

--- week2/scripts/train.py
+++ week2/scripts/train.py
@@ -1,9 +1,10 @@
 import os
 import pickle
 import click
-
+import mlflow
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
+from sklearn.metrics import accuracy_score
 
 
 def load_pickle(filename: str):
@@ -19,15 +20,23 @@ def load_pickle(filename: str):
 )
 def run_train(data_path: str):
 
-    X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
-    X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+    mlflow.set_experiment("homework2")
 
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_val)
+    with mlflow.start_run():
+        X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+        X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
-    rmse = mean_squared_error(y_val, y_pred, squared=False)
+        params = {"max_depth": 10, "random_state": 0}
+        mlflow.log_params(params)
 
+        rf = RandomForestRegressor(**params)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
+
+        rmse = mean_squared_error(y_val, y_pred, squared=False)
+        mlflow.log_metric("rmse",rmse)
+
+        mlflow.sklearn.log_model(rf, artifact_path="models")
 
 if __name__ == '__main__':
     run_train()
