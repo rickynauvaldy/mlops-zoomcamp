35ce5bbf0e1017a8294edf16915bf7e0f1a94c63
Ricky Nauvaldy <ricky.nauvaldy@gmail.com>
2023-06-03 00:03:15 +0700

    [W2] Update train, hpo, and register_model scripts

--- week2/scripts/train.py
+++ week2/scripts/train.py
@@ -4,7 +4,10 @@ import click
 import mlflow
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
-from sklearn.metrics import accuracy_score
+
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+mlflow.set_experiment("homework2")
+mlflow.sklearn.autolog()
 
 
 def load_pickle(filename: str):
@@ -16,17 +19,15 @@ def load_pickle(filename: str):
 @click.option(
     "--data_path",
     default="./output",
-    help="Location where the processed NYC taxi trip data was saved"
+    help="Location where the processed NYC taxi trip data was saved",
 )
 def run_train(data_path: str):
-
-    mlflow.set_experiment("homework2")
-
     with mlflow.start_run():
         X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
         X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
-        params = {"max_depth": 10, "random_state": 0}
+        # params = {"max_depth": 10, "random_state": 0}
+        params = {}
         mlflow.log_params(params)
 
         rf = RandomForestRegressor(**params)
@@ -34,9 +35,10 @@ def run_train(data_path: str):
         y_pred = rf.predict(X_val)
 
         rmse = mean_squared_error(y_val, y_pred, squared=False)
-        mlflow.log_metric("rmse",rmse)
+        mlflow.log_metric("rmse", rmse)
 
         mlflow.sklearn.log_model(rf, artifact_path="models")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     run_train()
